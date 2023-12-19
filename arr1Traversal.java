58172d38dac4ba1978212fcd32ff67907418c457
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2023-12-19 08:04:01 +0530

    Add files via upload

---
+++ arr1Traversal.java
@@ -0,0 +1,10 @@
+public class arr1Traversal {
+    public static void main(String[] args) {
+        int[] arr=new int[5];
+        System.out.println("The array elements are: ");
+        for (int i=0;i<5;i++){
+            arr[i]=i+2;
+            System.out.println("arr["+i+"}="+arr[i]);
+        }
+    }
+}
