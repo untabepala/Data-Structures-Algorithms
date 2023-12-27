419e06724f9c7c63108868750077c86c52bb663e
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2023-12-28 04:25:12 +0530

    Add files via upload

---
+++ prepend.java
@@ -0,0 +1,10 @@
+public class prepend/*(int data)*/ {
+    /* Create a new node with the given data
+    Node newHead = new Node(data);
+
+    // Set the next of the new node to the current head
+    newHead.next = head;
+
+    // Update the head to point to the new node, making it the new head
+    head = newHead;*/
+}
