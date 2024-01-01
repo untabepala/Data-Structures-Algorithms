5f8545f70411aa782f9f04fa91513c7083777671
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2024-01-02 04:49:16 +0530

    Add files via upload

---
+++ llimple1.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+import java.util.LinkedList;
+
+public class llimple1 {
+    public static void main(String[] args) {
+        LinkedList<String>ll=new LinkedList<>();
+        ll.add("A");
+        ll.add("B");
+        ll.add(1,"C");
+        System.out.println(ll);
+    }
+}
