4d3e060fdc14d4985e0d01e7f23811d5e329447b
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2024-01-09 01:25:44 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,23 +1,5 @@
-import java.util.LinkedList;
-
-public class StudentMarks {
-
+public class Main {
     public static void main(String[] args) {
-
-        // Create a linked list to store the students' Mathematics marks
-        LinkedList<Integer> marks = new LinkedList<>();
-
-        // Add the marks of 5 students to the linked list
-        marks.add(90);
-        marks.add(85);
-        marks.add(75);
-        marks.add(65);
-        marks.add(55);
-
-        // Display the students' Mathematics marks
-        System.out.println("The students' Mathematics marks are: ");
-        for (Integer mark : marks) {
-            System.out.println(mark);
-        }
+        System.out.println("Hello world!");
     }
 }
