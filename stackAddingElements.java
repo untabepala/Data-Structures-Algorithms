cec13ee25d49a506b284ba9590d2d80daccb3f62
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2024-01-11 01:49:50 +0530

    Add files via upload

---
+++ stackAddingElements.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class stackAddingElements {
+    public static void main(String[] args) {
+        Stack st1=new Stack();
+        Stack<String >st2=new Stack<String>();
+        st1.push("5");
+        st1.push("all");
+
+        st2.push("today");
+        System.out.println(st1);
+        System.out.println(st2);
+
+    }
+
+
+
+    }
+
+
